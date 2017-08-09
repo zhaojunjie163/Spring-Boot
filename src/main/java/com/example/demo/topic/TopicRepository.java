fc17417cf22cf8f3133a4484f13db3e22d1d706b
Guavaer <zhaojunjie.mike@gmail.com>
2017-08-09 10:15:40 +0800

    initial spring boot sample

--- src/main/java/com/example/demo/topic/TopicRepository.java
+++ src/main/java/com/example/demo/topic/TopicRepository.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
  * Created by zhaojunjie on 9/8/17.
  */
 
+/**
+ * you only need to declare interface, spring data JPA will provide implementation for you
+ */
+
+
 @Repository
 public interface TopicRepository extends CrudRepository<Topic,String> {
 
